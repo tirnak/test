c56472b17c055fa7c792a6a6192afec13eaeaa90
Kirill <semenovkirill.spb@gmail.com>
2016-06-27 19:32:11 +0300

    Create ex.c

---
+++ ex.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
+#include <string.h>
+#include <stdio.h>
+
+int main (void) {
+
+        struct { long type; char text[80]; } message;
+        int received;
+
+        /* Generate the ipc key */
+        key_t key = ftok("/tmp/msg.temp", 1);
+        printf("My key is %d\n", ipckey);
+
+        /* Set up the message queue */
+        // int mq_id = msgget(ipckey, 0);
+		int mq_id = msgget(ipckey, 0);
+        printf("Message identifier is %d\n", mq_id);
+
+        received = msgrcv(mq_id, &mymsg, sizeof(mymsg), 0, 0);
+		
+		FILE *f = fopen("/home/box/message.txt", "w");
+
+        fprintf(f, "%s%c", message.text, '\0');
+}
