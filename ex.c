1b83327cbc2bbcb5f5875a2953b5de158d2e11ee
Kirill <semenovkirill.spb@gmail.com>
2016-06-27 19:38:10 +0300

    Update ex.c

--- ex.c
+++ ex.c
@@ -11,14 +11,14 @@ int main (void) {
 
         /* Generate the ipc key */
         key_t key = ftok("/tmp/msg.temp", 1);
-        printf("My key is %d\n", ipckey);
+        printf("My key is %d\n", key);
 
         /* Set up the message queue */
         // int mq_id = msgget(ipckey, 0);
 		int mq_id = msgget(ipckey, 0);
         printf("Message identifier is %d\n", mq_id);
 
-        received = msgrcv(mq_id, &mymsg, sizeof(mymsg), 0, 0);
+        received = msgrcv(mq_id, &message, sizeof(message), 0, 0);
 		
 		FILE *f = fopen("/home/box/message.txt", "w");
 
