d19c79e321e2dba2c1ce873c47dc90bd0e93c612
Kirill <semenovkirill.spb@gmail.com>
2016-06-29 15:47:55 +0300

    Update ex.c

--- ex.c
+++ ex.c
@@ -1,28 +1,18 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/stat.h>
-#include <sys/msg.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <mqueue.h>     /* message queue stuff */
+#include <errno.h>      /* errno and perror */
+#include <fcntl.h>      /* O_RDONLY */
 #include <stdio.h>
-#include <errno.h>
-#include <mqueue.h>
 #define MAX_SIZE 80
 
 struct mq_attr attr;
-
-
-
-attr.mq_flags = 0;
-attr.mq_maxmsg = 10;
-attr.mq_msgsize = MAX_SIZE;
-attr.mq_curmsgs = 0;
-
 char buffer[MAX_SIZE];
 
 int main()
 {
-	
+	attr.mq_flags = 0;
+	attr.mq_maxmsg = 10;
+	attr.mq_msgsize = MAX_SIZE;
+	attr.mq_curmsgs = 0;
 	mqd_t mq = mq_open("/test.mq", O_CREAT | O_RDWR, 0666,);
 	if( mq == -1 ) {
 		printf("error: %s", strerror(errno));
