59f46a870d0ec8d241852e8e3e75436b43fe73b1
Kirill <semenovkirill.spb@gmail.com>
2016-06-29 16:39:20 +0300

    Update ex.c

--- ex.c
+++ ex.c
@@ -15,13 +15,16 @@ int main()
 	attr.mq_maxmsg = 10;
 	attr.mq_msgsize = MAX_SIZE;
 	attr.mq_curmsgs = 0;
+	printf("mq_msgsize is %s", attr.mq_msgsize);
+	
 	mqd_t mq = mq_open("/test.mq", O_CREAT | O_RDWR, 0666, &attr);
 	if( mq == -1 ) {
 		printf("mq. error: %s", strerror(errno));
 		return -1;
 	}
 	memset(buffer, 0, MAX_SIZE+1);
-	int received = mq_receive(mq, buffer, MAX_SIZE + 1, 0);
+	printf("buffer is %s", buffer);
+	int received = mq_receive(mq, buffer, MAX_SIZE + 1, NULL);
 	if( received == -1 ) {
 		printf("receive. error: %s", strerror(errno));
 		return -1;
