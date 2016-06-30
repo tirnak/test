3810ed90c5f35579290b7ccf10fefdeb96f69aeb
Kirill <semenovkirill.spb@gmail.com>
2016-06-30 18:42:15 +0300

    Update ex.c

--- ex.c
+++ ex.c
@@ -24,10 +24,10 @@ int main()
       int shmid;
       int new = 1; 
       
-      shmid = shm_open("/test.shm", O_CREAT|O_RDWR, 0666)
+      shmid = shm_open("/test.shm", O_CREAT|O_RDWR, 0666);
       check(shmid, "key");
       int trunc = ftruncate(shmid, 1048576);
-      check(trunc, "trunc")
+      check(trunc, "trunc");
       char *ptr = mmap(0,1048576, PROT_READ | PROT_WRITE, MAP_SHARED, shmid, 0);
       memset(ptr, 13, 1048576);
       return 0;
