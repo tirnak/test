32daaaaa56bf488bc7753401c17b4020b3b2917d
Kirill <semenovkirill.spb@gmail.com>
2016-07-01 16:39:21 +0300

    Update ex.c

--- ex.c
+++ ex.c
@@ -19,29 +19,68 @@ void clearBuf(char* buf) {
 	memset(buf, 0, sizeof(buf));
 }
 
-void* helloWorld(void *args) {
-    printf("Hello from thread!\n");
-    pause();
-    return 0;
+pthread_mutex_t mutex;
+pthread_spinlock_t spinlock;
+pthread_rwlock_t rwlock;
+
+void mutex(void *args) {
+    printf("Hello from mutex!\n");
+    pthread_mutex_lock(&mutex);
+}
+
+void spin(void *args) {
+    printf("Hello from spinlock!\n");
+    spin_lock(&spinlock);
+}
+
+void read(void *args) {
+    printf("Hello from read!\n");
+    pthread_rwlock_rdlock(&rwlock);
+}
+
+void write(void *args) {
+    printf("Hello from write!\n");
+    pthread_rwlock_wrlock(&rwlock);
 }
  
 int main() {
-    pthread_t thread;
+    pthread_t threadMutex,threadSpin,threadR,threadW;
+    
     int status;
-    int status_addr;
     FILE *f = fopen("/home/box/main.pid", "w+");
     // check((int) f, "file");
     fprintf(f, "%d\n", getpid());
+    
+    status = pthread_mutex_init(&lock, NULL);
+    check(status, "mutex init");
+    pthread_mutex_lock(&mutex);
+    
+    status = pthread_spin_init(&lock, PTHREAD_PROCESS_SHARED); 
+    check(status, "spinlock init");
+    spin_lock(&spinlock);
+    
+    status = pthread_rwlock_init(rwlock, NULL);
+    check(status, "rwlock init");
+    status = pthread_rwlock_rdlock(&rwlock);
+    check(status, "rwlock read lock");
+    status = pthread_rwlock_wrlock(&rwlock);
+    check(status, "rwlock write lock");
+
+    
+    status = pthread_create(&threadMutex, NULL, &mutex, NULL);
+    check(status, " create mutex thread");
+    
+    status = pthread_create(&threadSpin, NULL, &spin, NULL);
+    check(status, " create spin thread");
  
-    status = pthread_create(&thread, NULL, helloWorld, NULL);
-    check(status, "create");
+    status = pthread_create(&threadR, NULL, read, NULL);
+    check(status, "create read");
+ 
+    status = pthread_create(&threadW, NULL, write, NULL);
+    check(status, "create write");
+    
     printf("Hello from main!\n");
  
-    status = pthread_join(thread, (void**)&status_addr);
-    check(status, "create");
- 
-    printf("joined with address %d\n", status_addr);
-    int i;
     std::cin >> i;
     return 0;
 }
