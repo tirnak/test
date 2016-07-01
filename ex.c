2169438815f0670c281c9dec638887d4e405f3a1
Kirill <semenovkirill.spb@gmail.com>
2016-07-01 13:01:16 +0300

    Update ex.c

--- ex.c
+++ ex.c
@@ -1,10 +1,8 @@
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
 
 void check(int toCheck, char* msg) {
 	if (toCheck == -1) {
@@ -19,16 +17,28 @@ void clearBuf(char* buf) {
 	memset(buf, 0, sizeof(buf));
 }
 
-int main()
-{
-      int shmid;
-      int new = 1; 
-      
-      shmid = shm_open("/test.shm", O_CREAT|O_RDWR, 0666);
-      check(shmid, "key");
-      int trunc = ftruncate(shmid, 1048576);
-      check(trunc, "trunc");
-      char *ptr = mmap(0,1048576, PROT_READ | PROT_WRITE, MAP_SHARED, shmid, 0);
-      memset(ptr, 13, 1048576);
-      return 0;
+void* helloWorld(void *args) {
+    printf("Hello from thread!\n");
+    pause();
+    return 0;
+}
+ 
+int main() {
+    pthread_t thread;
+    int status;
+    int status_addr;
+    FILE *f = fopen("", "w+");
+    check((int) f, "file");
+    fprintf("%d\n", getpid());
+ 
+    status = pthread_create(&thread, NULL, helloWorld, NULL);
+    check(status, "create");
+    printf("Hello from main!\n");
+ 
+    status = pthread_join(thread, (void**)&status_addr);
+    check(status, "create");
+ 
+    printf("joined with address %d\n", status_addr);
+    _getch();
+    return 0;
 }
